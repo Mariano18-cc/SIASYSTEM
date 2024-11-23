88404b629447709d3a388eb98f1108bbec1dd1ff
rayfle <rayfledc09@gmail.com>
2024-11-24 00:14:32 +0800

    fix employee css path

--- Admin/employee.php
+++ Admin/employee.php
@@ -125,7 +125,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['employee_id']) && isse
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employee Management</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="../stylesheet/employee.css">
+    <link rel="stylesheet" href="css/employee.css">
 </head>
 <style>
     .schedule-button {
